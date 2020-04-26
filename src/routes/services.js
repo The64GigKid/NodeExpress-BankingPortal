2306f1cd325a9666ceb053911703e3ad38e01e7f
The64GigKid <47188661+The64GigKid@users.noreply.github.com>
2020-04-26 13:28:25 -0600

    Update services.js

--- src/routes/services.js
+++ src/routes/services.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-const { accounts } = require('../data.js');
-
 const { accounts, writeJSON } = require('../data.js');
 
 router.get('/transfer', (req, res) =>  res.render('transfer'));
